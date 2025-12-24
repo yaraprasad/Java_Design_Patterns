f63ab3b920fb33b5df82449192f0c9c267afe470
Prasad Yara <prasad.yara2779@gmail.com>
2025-12-24 15:19:16 +0530

    initial commit;

--- src/com/java/basic/design/BasicDesignPatterns.java
+++ src/com/java/basic/design/BasicDesignPatterns.java
@@ -1,9 +1,9 @@
-package com.java.collections;
+package com.java.basic.design;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class BasicCollections {
+public class BasicDesignPatterns{
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("India","Pakistan");
